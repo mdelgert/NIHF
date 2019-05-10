b4f819648cbcfe26654e0d8cf86cd0f6ffb57728
mdelgert <mdelgert@vessea.com>
2019-05-10 14:21:22 -0400

    added database scripts

---
+++ Database/SampleData.sql
@@ -0,0 +1,14 @@
+﻿USE [NIHF]
+GO
+SET IDENTITY_INSERT [dbo].[Manufacturer] ON 
+
+INSERT [dbo].[Manufacturer] ([ID], [Name]) VALUES (1, N'Delco')
+INSERT [dbo].[Manufacturer] ([ID], [Name]) VALUES (2, N'Chrysler')
+INSERT [dbo].[Manufacturer] ([ID], [Name]) VALUES (3, N'Dodge')
+INSERT [dbo].[Manufacturer] ([ID], [Name]) VALUES (5, N'FORD')
+SET IDENTITY_INSERT [dbo].[Manufacturer] OFF
+SET IDENTITY_INSERT [dbo].[Part] ON 
+
+INSERT [dbo].[Part] ([ID], [Number], [Name], [Description], [ManufacturerID]) VALUES (1, N'00001', N'SPK', N'Ignites gas in the engine cylinder', 1)
+INSERT [dbo].[Part] ([ID], [Number], [Name], [Description], [ManufacturerID]) VALUES (3, N'00003', N'RRP', N'Truck Right Rear Passenger Panel', 1)
+SET IDENTITY_INSERT [dbo].[Part] OFF
---
+++ Database/dbo.Part.sql
@@ -0,0 +1,24 @@
+﻿USE [NIHF]
+GO
+
+/****** Object:  Table [dbo].[Part]    Script Date: 5/10/2019 2:17:56 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Part](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Number] [nvarchar](50) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](50) NOT NULL,
+	[ManufacturerID] [int] NOT NULL,
+ CONSTRAINT [PK_Part] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
